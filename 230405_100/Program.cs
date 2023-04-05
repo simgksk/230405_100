c64290b2657c5c6a9cd00767e39de256bd69b499
simgksk <Daum_c0m@naver.com>
2023-04-05 16:59:17 +0900

    초기 커밋.

---
+++ 230405_100/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _230405_100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str = Console.ReadLine().Split();
+            int a = int.Parse(str[0]);
+            int b = int.Parse(str[1]);
+            int c = int.Parse(str[2]);
+
+            Console.WriteLine((a+b)%c);
+            Console.WriteLine((a%c)+(b%c)%c);
+            Console.WriteLine((a*b)%c);
+            Console.WriteLine((a%c)*(b%c)%c);
+        }
+    }
+}
